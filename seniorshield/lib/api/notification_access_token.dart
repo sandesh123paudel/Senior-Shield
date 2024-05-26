1b2b07ae612684536256f22cdd0a767621885aa8
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2024-05-26 23:44:21 +0545

    Completed Project

--- seniorshield/lib/api/notification_access_token.dart
+++ seniorshield/lib/api/notification_access_token.dart
@@ -18,19 +18,7 @@ class NotificationAccessToken {
 
       final serviceAccountCredentials = ServiceAccountCredentials.fromJson(
         jsonDecode(r'''
-{
-  "type": "service_account",
-  "project_id": "seniorshieldapp-a65aa",
-  "private_key_id": "",
-  "private_key": "",
-  "client_email": "",
-  "client_id": "",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-elhl4%40seniorshieldapp-a65aa.iam.gserviceaccount.com",
-  "universe_domain": "googleapis.com"
-}
+
 '''),
       );
 
